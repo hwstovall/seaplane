dc3b3a9c43259b84fb45fb470e62bc434fa66533
hwstovall <hwstovall@gmail.com>
2018-02-27 16:37:19 -0500

    Still figuring this out.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='seaplane',
-      version='0.1.1',
+      version='0.1.2',
       description='A simple tool to help get local Kubernetes projects up and running.',
       url='https://github.com/hwstovall/seaplane',
-      download_url='https://github.com/hwstovall/seaplane/archive/0.1.1.tar.gz',
+      download_url='https://github.com/hwstovall/seaplane/archive/0.1.2.tar.gz',
       author='hwstovall',
       author_email='hwstovall@gmail.com',
       license='MIT',
